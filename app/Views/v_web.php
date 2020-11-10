9cd85261e34c9fdb43ba3053fa0598f62f5b0ad8
Imsan Surya <muhammadimsans@gmail.com>
2020-11-10 16:38:56 +0700

    4pm

--- app/Views/v_web.php
+++ app/Views/v_web.php
@@ -37,31 +37,14 @@
     <nav class="navbar navbar-static-top">
       <div class="container">
         <div class="navbar-header">
-          <a href="../../index2.html" class="navbar-brand"><b>Toba</b>Research</a>
+          <a href="login" class="navbar-brand"><b>Toba</b>Research</a>
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
             <i class="fa fa-bars"></i>
           </button>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Link</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li class="divider"></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+       
         <!-- /.navbar-collapse -->
         <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
@@ -99,11 +82,8 @@
   <!-- /.content-wrapper -->
   <footer class="main-footer">
     <div class="container">
-      <div class="pull-right hidden-xs">
-        <b>Version</b> 2.4.18
-      </div>
-      <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
-      reserved.
+      
+      <strong>Copyright &copy; 2020 </strong>
     </div>
     <!-- /.container -->
   </footer>
